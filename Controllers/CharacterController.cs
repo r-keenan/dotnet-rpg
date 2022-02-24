6b092056cd3e315a3403c4bed9f5c3045dfa8289
rkeenan <thero@DESKTOP-T4F6H9S>
2022-02-24 09:59:03 -0600

    Moved this into a service and called the service in the program file.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,8 @@
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using dotnet_rpg.Services.CharacterService;
+using dotnet_rpg.Controllers;
 
 namespace dotnet_rpg.Controllers
 {
@@ -10,30 +12,32 @@ namespace dotnet_rpg.Controllers
     //ControllerBase is used when you do not want a View implemented. Controller is used when including a View.
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>{
-            new Character(),
-            new Character { Id = 1, Name = "Sam"}
-        };
+
+        //Constructor
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
         //If decorater is not added, then you can get the "Ambiguous HTTP method for Action" error.
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
             //status code 200 for (Ok)
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault( c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using dotnet_rpg.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
