b41c1afd23b5a7008661d085af179436a5496626
rkeenan <thero@DESKTOP-T4F6H9S>
2022-02-23 18:36:01 -0600

    Created basic model for Character

---
+++ Models/Character.cs
@@ -0,0 +1,23 @@
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id {get; set;}
+        // put in an equal sing and then the data type is for setting a default value
+        public string Name { get; set; } = "Frodo";
+
+        public int HitPoints {get; set;} = 100;
+
+        public int Strength { get; set; } = 10;
+
+        public int Defense { get; set; } = 10;
+
+        public int Intelligence {get; set;} = 10;
+        
+        
+        
+        
+        
+            
+    }
+}
