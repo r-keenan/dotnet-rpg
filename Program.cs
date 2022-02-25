17d5f72935667f1e5bda38360810084a80af317e
rkeenan <thero@DESKTOP-T4F6H9S>
2022-02-24 20:07:57 -0600

    Figured out how to the difference in code between .Net 5 & 6 for SQL Server connection. Ran migration as well. Successfully ran migration from terminal.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-using dotnet_rpg.Services.CharacterService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
 using AutoMapper;
+using dotnet_rpg.Data;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// in .Net Core 5, this would be:
+// builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
