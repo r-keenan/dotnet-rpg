02fa531e82c6c1ae81a5c6c5021c6f4766aba753
rkeenan <thero@DESKTOP-T4F6H9S>
2022-03-12 20:54:26 -0600

    missed new dto file in the commit.

---
+++ Dtos/Character/GetCharacterNameOnlyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_rpg.Dtos.Character
+{
+    public class GetCharacterNameOnlyDto
+    {
+        public int Id { get; set; }
+        // put in an equal sing and then the data type is for setting a default value
+        public string Name { get; set; } = "Frodo";
+    }
+}
